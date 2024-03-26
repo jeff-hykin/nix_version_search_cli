ab71174fc2fcb6426896aed45c011a8986d73c2a
Jeff Hykin <jeff.hykin@gmail.com>
2024-03-26 18:12:03 -0500

    build

--- default.nix
+++ default.nix
@@ -30,7 +30,7 @@
     _core.derivation {
         system = system;
         name = "nvs";
-        version = "1.3.11";
+        version = "1.3.12";
         builder = "${bash}/bin/bash";
         src = _src;
         args = [
