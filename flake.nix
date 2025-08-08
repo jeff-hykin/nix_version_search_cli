34e26e283e3ccedd9c528046fd2d89f8d86976e2
Jeff Hykin <jeff.hykin@gmail.com>
2025-08-08 13:55:28 -0500

    add xome for debugging

--- flake.nix
+++ flake.nix
@@ -4,9 +4,10 @@
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
         flakeUtils.url = "github:numtide/flake-utils";
+        xome.url = "github:jeff-hykin/xome";
     };
 
-    outputs = { self, nixpkgs, flakeUtils }:
+    outputs = { self, nixpkgs, flakeUtils, xome }:
         flakeUtils.lib.eachDefaultSystem (system:
             let
                 pkgs = nixpkgs.legacyPackages.${system};
@@ -20,6 +21,71 @@
                     packages = {
                         nvs = nvs;
                         default = nvs;
+                    };
+                    devShells = xome.simpleMakeHomeFor {
+                        inherit pkgs;
+                        pure = true;
+                        homeModule = {
+                            # for home-manager examples, see: 
+                            # https://deepwiki.com/nix-community/home-manager/5-configuration-examples
+                            # all home-manager options: 
+                            # https://nix-community.github.io/home-manager/options.xhtml
+                            home.homeDirectory = "/tmp/virtual_homes/nix_version_search_cli";
+                            home.stateVersion = "25.11";
+                            home.packages = [
+                                # vital stuff
+                                pkgs.nix
+                                pkgs.coreutils-full
+                                
+                                # optional stuff
+                                pkgs.gnugrep
+                                pkgs.findutils
+                                pkgs.wget
+                                pkgs.curl
+                                pkgs.unixtools.locale
+                                pkgs.unixtools.more
+                                pkgs.unixtools.ps
+                                pkgs.unixtools.getopt
+                                pkgs.unixtools.ifconfig
+                                pkgs.unixtools.hostname
+                                pkgs.unixtools.ping
+                                pkgs.unixtools.hexdump
+                                pkgs.unixtools.killall
+                                pkgs.unixtools.mount
+                                pkgs.unixtools.sysctl
+                                pkgs.unixtools.top
+                                pkgs.unixtools.umount
+                                pkgs.git
+                                pkgs.htop
+                                pkgs.ripgrep
+                                # project specific stuff
+                                pkgs.patchelf
+                                nvs
+                            ];
+                            
+                            programs = {
+                                home-manager = {
+                                    enable = true;
+                                };
+                                zsh = {
+                                    enable = true;
+                                    enableCompletion = true;
+                                    autosuggestion.enable = true;
+                                    syntaxHighlighting.enable = true;
+                                    shellAliases.ll = "ls -la";
+                                    history.size = 100000;
+                                    # this is kinda like .zshrc
+                                    initContent = ''
+                                        # this enables some impure stuff like sudo, comment it out to get FULL purity
+                                        export PATH="$PATH:/usr/bin/"
+                                    '';
+                                };
+                                starship = {
+                                    enable = true;
+                                    enableZshIntegration = true;
+                                };
+                            };
+                        }; 
                     };
                 }
         );
